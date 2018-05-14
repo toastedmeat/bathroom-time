2d17cedaf7542bbeb98c816072bbec531013acc1
toastedmeat <eloo12@gmail.com>
2018-05-14 19:43:44 -0400

    Remove JPA dependency opting for Mongo

--- server/src/main/java/com/el/apps/Bathroom/repositories/UserRepository.java
+++ server/src/main/java/com/el/apps/Bathroom/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.el.apps.Bathroom.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -7,4 +9,7 @@ import com.el.apps.Bathroom.models.User;
 
 @RepositoryRestResource
 public interface UserRepository extends MongoRepository<User, String> {
+	
+	public User findByFirstName(String firstName);
+	public List<User> findByLastName(String lastName);
 }
