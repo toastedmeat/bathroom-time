d5b9b181c3feaf43b7d8a02362dfc6fa1c959d27
toastedmeat <eloo12@gmail.com>
2018-05-16 22:42:03 -0400

    Added skeletons for addtional models

---
+++ server/src/main/java/com/el/apps/Bathroom/models/Picture.java
@@ -0,0 +1,11 @@
+package com.el.apps.Bathroom.models;
+
+import org.springframework.data.annotation.Id;
+
+public class Picture {
+	
+	@Id
+    private String id;
+    private String url;
+
+}
---
+++ server/src/main/java/com/el/apps/Bathroom/models/Tag.java
@@ -0,0 +1,11 @@
+package com.el.apps.Bathroom.models;
+
+import org.springframework.data.annotation.Id;
+
+public class Tag {
+	
+	@Id
+    private String id;
+    private String name;
+
+}
