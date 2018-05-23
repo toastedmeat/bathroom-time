0a8ae547a64846f02b4ebd6c1d86d54ad6e9109b
toastedmeat <eloo12@gmail.com>
2018-05-23 08:25:02 -0400

    Added skeleton for review api
    Changed styling to java camelcase
    
    Added review controller
    
    Added review repo
    
    use dbrefs for foreign keys

---
+++ server/src/main/java/com/el/apps/Bathroom/controllers/ReviewController.java
@@ -0,0 +1,95 @@
+package com.el.apps.Bathroom.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.el.apps.Bathroom.models.Review;
+import com.el.apps.Bathroom.repositories.ReviewRepository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5000"})
+public class ReviewController {
+    
+    @Autowired
+    private ReviewRepository repository;
+    
+    // Get all
+    @GetMapping("/review/")
+    public ResponseEntity<Collection<Review>> getAllUsers() {
+        Collection<Review> reviews = repository.findAll().stream().collect(Collectors.toList());
+        if(reviews.isEmpty())
+            return new ResponseEntity<Collection<Review>>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Collection<Review>>(reviews, HttpStatus.OK);
+    }
+    
+    // get single
+    @GetMapping("/review/{id}")
+    public ResponseEntity<?> getReviewById(@PathVariable("id") String id) {
+        Optional<Review> review = repository.findById(id);
+        if(!review.isPresent())
+            return new ResponseEntity<String>("Review with id { " + id + " }" +
+                    " not found", HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Review>(review.get(), HttpStatus.OK);
+    }
+    
+    // post
+    @PostMapping("/review/")
+    public ResponseEntity<?> createReview(@RequestBody Review review){
+        if(repository.findById(review.getId()) != null)
+            return new ResponseEntity<String>("Unable to create review, review with id " +
+                    "{ " + review.getId() + " } already exists", HttpStatus.CONFLICT);
+        Review newReview = repository.save(review);
+
+        return new ResponseEntity<Review>(newReview, HttpStatus.CREATED);
+    }
+    
+    // update
+    @PutMapping("/review/{id}")
+    public ResponseEntity<?> updateReview(@PathVariable("id") String id, @RequestBody Review updates){
+        Optional<Review> review = repository.findById(id);
+        if(!review.isPresent())
+            return new ResponseEntity<String>("Review with id { " + id + " }" +
+                    " not found", HttpStatus.NOT_FOUND);
+        review.get().setRating(updates.getRating());
+        review.get().setDescription(updates.getDescription());
+
+        repository.save(review.get());
+        return new ResponseEntity<Review>(review.get(), HttpStatus.OK);
+    }
+    
+    // delete single
+    @DeleteMapping("/review/{id}")
+    public ResponseEntity<?> deleteReview(@PathVariable("id") String id){
+        Optional<Review> review = repository.findById(id);
+        if(!review.isPresent())
+            return new ResponseEntity<String>("Review with id { " + id + " }" +
+                    " not found", HttpStatus.NOT_FOUND);
+        repository.delete(review.get());
+        return new ResponseEntity<Review>(HttpStatus.NO_CONTENT);
+    }
+    
+    // delete all by bathroom id
+    @DeleteMapping("/review/{bathroomId}")
+    public ResponseEntity<?> deleteAllReviewByBathroomId(@PathVariable("bathroomId") String bathroomId){
+        List<Review> reviews = repository.findByBathroomId(bathroomId);
+        if(reviews.isEmpty())
+            return new ResponseEntity<String>("Review with bathroomId { " + bathroomId + " }" +
+                    " not found", HttpStatus.NOT_FOUND);
+        repository.deleteByBathroomId(bathroomId);
+        return new ResponseEntity<Review>(HttpStatus.NO_CONTENT);
+    }
+}
--- server/src/main/java/com/el/apps/Bathroom/models/Review.java
+++ server/src/main/java/com/el/apps/Bathroom/models/Review.java
@@ -1,73 +1,94 @@
 package com.el.apps.Bathroom.models;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 public class Review {
-	
-	@Id
-  private String id;
-	private String user_id;
-  private String bathroom_id;
-  private String rating;
-  private String description;
-  private Review[] children;
+    
+    @Id
+    private String id;
+    @DBRef
+    private User user;
+    @DBRef
+    private Bathroom bathroom;
+    private String rating;
+    private String description;
+    private List<Review> children;
 
-  public Review() {}
+    public Review() {}
 
-  public Review(String description, String rating) {
+    public Review(String description, String rating) {
       this.description = description;
       this.rating = rating;
-  }
-    
-  public String getUser_id() {
-		return user_id;
-	}
+    }
 
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
+    public Review(String description, String rating, User user, Bathroom bathroom, List<Review> children) {
+      this.description = description;
+      this.rating = rating;
+      this.user = user;
+      this.bathroom = bathroom;
+      this.children = children;
+    }
 
-	public String getBathroom_id() {
-		return bathroom_id;
-	}
-
-	public void setBathroom_id(String bathroom_id) {
-		this.bathroom_id = bathroom_id;
-	}
-
-	public String getRating() {
-		return rating;
-	}
-
-	public void setRating(String rating) {
-		this.rating = rating;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Review[] getChildren() {
-		return children;
-	}
-
-	public void setChildren(Review[] children) {
-		this.children = children;
-	}
-
-  @Override
-  public String toString() {
+    @Override
+    public String toString() {
       return "Rating { " +
       "id=" + id +
-      "user_id=" + user_id +
-      "bathroom_id=" + bathroom_id +
+      "user=" + user +
+      "bathroom=" + bathroom +
       "rating=" + rating +
       "description=" + description +
       "children=" + children +
       " }";
-  }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Review> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Review> children) {
+        this.children = children;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Bathroom getBathroom() {
+        return bathroom;
+    }
+
+    public void setBathroom(Bathroom bathroom) {
+        this.bathroom = bathroom;
+    }
 }
