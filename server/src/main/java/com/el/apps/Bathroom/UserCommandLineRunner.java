e2f88564492613358eae1bd5bd1e9b0ae9d1b1cf
toastedmeat <eloo12@gmail.com>
2018-05-23 07:46:48 -0400

    Change tabs to spaces

--- server/src/main/java/com/el/apps/Bathroom/UserCommandLineRunner.java
+++ server/src/main/java/com/el/apps/Bathroom/UserCommandLineRunner.java
@@ -7,8 +7,8 @@ import com.el.apps.Bathroom.repositories.UserRepository;
 
 @Component
 public class UserCommandLineRunner implements CommandLineRunner {
-	
-	
+    
+    
     private final UserRepository users;
 
     public UserCommandLineRunner(UserRepository users) {
@@ -17,37 +17,37 @@ public class UserCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-    	System.out.println("Running User Init");
+        System.out.println("Running User Init");
         users.deleteAll();
-		
-		users.save(new User("eloo", "Eric", null, "Loo", "eloo12@gmail.com"));
-		users.save(new User("jzeng", "Jessica", null, "Zeng", "jessicazengg@gmail.com"));
-		users.save(new User("hqu", "Haibin", null, "qu", "haibinqu@hotmail.com"));
-		users.save(new User("bqu", "Bob", null, "qu", "bobqu@hotmail.com"));
-		users.save(new User("hqu", "Harris", null, "qu", "harrisqu@hotmail.com"));
-		
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (User user : users.findAll()) {
-			System.out.println(user);
-		}
-		System.out.println();
+        
+        users.save(new User("eloo", "Eric", null, "Loo", "eloo12@gmail.com"));
+        users.save(new User("jzeng", "Jessica", null, "Zeng", "jessicazengg@gmail.com"));
+        users.save(new User("hqu", "Haibin", null, "qu", "haibinqu@hotmail.com"));
+        users.save(new User("bqu", "Bob", null, "qu", "bobqu@hotmail.com"));
+        users.save(new User("hqu", "Harris", null, "qu", "harrisqu@hotmail.com"));
+        
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (User user : users.findAll()) {
+            System.out.println(user);
+        }
+        System.out.println();
 
-		// fetch an individual user
-		System.out.println("User found with findByFirstName('Jessica'):");
-		System.out.println("--------------------------------");
-		System.out.println(users.findByFirstNameAllIgnoreCase("Jessica"));
-		
-		// fetch an individual user not found
-		System.out.println("User found with findByFirstName('Jessicah'):");
-		System.out.println("--------------------------------");
-		System.out.println(users.findByUsernameAllIgnoreCase("Jessicah"));
+        // fetch an individual user
+        System.out.println("User found with findByFirstName('Jessica'):");
+        System.out.println("--------------------------------");
+        System.out.println(users.findByFirstNameAllIgnoreCase("Jessica"));
+        
+        // fetch an individual user not found
+        System.out.println("User found with findByFirstName('Jessicah'):");
+        System.out.println("--------------------------------");
+        System.out.println(users.findByUsernameAllIgnoreCase("Jessicah"));
 
-		// find multiple users with last name qu
-		System.out.println("Users found with findByLastName('Qu'):");
-		System.out.println("--------------------------------");
-		for (User user : users.findByLastNameAllIgnoreCase("qu")) {
-			System.out.println(user);
-		}
+        // find multiple users with last name qu
+        System.out.println("Users found with findByLastName('Qu'):");
+        System.out.println("--------------------------------");
+        for (User user : users.findByLastNameAllIgnoreCase("qu")) {
+            System.out.println(user);
+        }
     }
 }
