05bc3813fdf571f4d669b2f0076ce6e0f311019f
toastedmeat <eloo12@gmail.com>
2018-05-25 18:49:00 -0400

    starting Tag endpoint

--- server/src/main/java/com/el/apps/Bathroom/models/Tag.java
+++ server/src/main/java/com/el/apps/Bathroom/models/Tag.java
@@ -7,5 +7,21 @@ public class Tag {
     @Id
     private String id;
     private String name;
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
